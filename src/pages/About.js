62cba4184358106fb85453c70b7adda24a6be2e8
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-06-23 22:58:25 +0200

    1er projet react

---
+++ src/pages/About.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+        explicabo maiores quas incidunt facilis alias nam dicta accusamus. Quis
+        exercitationem possimus laborum similique tempora optio provident
+        aperiam corporis placeat, molestiae libero. Eum expedita consequatur
+        pariatur! Neque iure magni sit. Culpa ullam magni tempore voluptatum
+        nulla pariatur facilis repellat adipisci ea totam quam minima illo,
+        placeat numquam ab officiis est, iste accusantium sapiente. Aspernatur
+        nisi facilis aperiam. Consectetur temporibus sapiente, tempore est
+        molestias et? Cumque animi itaque veritatis non quam, laborum eius
+        pariatur qui sunt vel, facere, a sit numquam? Ab veniam assumenda
+        molestiae maxime. Doloribus reiciendis voluptate ab sequi impedit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
+        similique, cupiditate officiis ex laboriosam sapiente ipsum itaque.
+        Earum perspiciatis, sit optio facere blanditiis officia nobis voluptates
+        dolores, id tempore repellat ad. Quas quos animi fugit veritatis eius
+        asperiores error earum rem, molestias fugiat, minima, nesciunt nam vero
+        corporis? Adipisci obcaecati ipsa inventore voluptate saepe illo
+        expedita, corrupti veritatis incidunt pariatur modi? Tempore obcaecati
+        sint numquam quaerat libero atque iure, distinctio, illo aut ullam a
+        quos, ipsa excepturi reprehenderit laudantium necessitatibus debitis.
+        Dicta, nostrum optio. Voluptate fugiat sed adipisci maxime aliquid
+        provident, dolor saepe sapiente nostrum, doloremque ratione. Placeat
+        similique eaque nihil molestiae voluptate repellat, veritatis mollitia.
+        Est, omnis nostrum tempore quia debitis provident illo soluta sapiente
+        quas, suscipit maiores laborum et, molestias delectus laudantium
+        dignissimos quis voluptatem perferendis totam. Facere, a. Ducimus ipsum
+        et, minima, enim quasi sapiente mollitia explicabo nemo ea labore
+        consectetur. Doloribus nostrum est laboriosam quae obcaecati harum
+        tenetur voluptate perferendis, consequuntur nam maiores nobis amet aut
+        quis eos totam. Ad beatae assumenda optio, unde illo harum consectetur
+        dolores eaque excepturi accusamus velit? Rerum laudantium fuga quasi
+        placeat nesciunt quidem provident sit, optio culpa voluptas harum.
+        Voluptas ratione consequatur vitae et. Dolore quia soluta odit enim
+        eaque?
+      </p>
+    </div>
+  );
+};
+
+export default About;
