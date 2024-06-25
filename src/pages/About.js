859c9dd4d9c055d85b83269b87c469ef7812a805
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-06-25 23:33:21 +0200

    About et Home terminées

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
 
 const About = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
